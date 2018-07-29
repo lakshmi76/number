88f60c8c4016106c3fd5c7eb5aa5a80705b7e949
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-29 14:06:44 +0530

    Rename Multi.java to Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,19 @@
+class Multiplication
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int n,i=1,temp=0;
+		 Scanner sc=new Scanner(System.in);
+		 n=sc.nextInt();
+		
+		while(i<=5)
+		{
+			temp=n*i;
+			System.out.println("number is" +temp);
+	                        i=i+1;
+	                        
+		}
+		
+		
+	}
+}
