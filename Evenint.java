fc6e228de884175ff0cf2f2b3ffe7ffeba2bd0a5
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-29 13:48:17 +0530

    Create Evenint.java

---
+++ Evenint.java
@@ -0,0 +1,18 @@
+class evenint
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num,m,n;
+	Scanner sc=new Scanner(System.in);
+	m=sc.nextInt();
+	n=sc.nextInt();
+for(num=m;num<=n;num++)
+{
+	if(num%2==0)
+	{
+		System.out.println("number is" +num);
+	}
+// your code goes here
+}
+}
+}
