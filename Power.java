5ad9269297d5b8fdef1a00ed7acc43e93df969e5
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-29 13:35:55 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,17 @@
+class Power
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+
+   
+
+        int base , exponent ;
+        Scanner sc=new Scanner(System.in);
+        base= sc.nextInt();
+        exponent=sc.nextInt();
+        double result = Math.pow(base, exponent);
+
+        System.out.println("Answer = " + result);// your code goes here
+	}
+}
