9a22e451f37da5a6727bd87d323c9c8fd82661d3
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-29 13:24:21 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+{
+int i,n;
+Scanner sc=new Scannner(System.in);
+n=sc.nextInt();
+for(i=1;i<=n;i++)
+{
+System.out.println("hello world");
+}
+}
+}
