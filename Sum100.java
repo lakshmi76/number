a1c6feb490402c1805471cb1bf8b31e81cc684f5
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-29 13:27:57 +0530

    Create Sum100.java

---
+++ Sum100.java
@@ -0,0 +1,12 @@
+class Sum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int sum=0,num=100;
+	for(int i=0;i<=num;i++)
+	{
+		sum=sum+i;
+	}
+	System.out.println("sum:" +sum);
+	}// your code goes here
+	}
