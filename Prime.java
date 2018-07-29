5ccf64cb4a7a8f5ddbc7ecc23652186c98af4c3f
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-29 13:43:20 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,22 @@
+class Prime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int num,i=2;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+if(i==2)
+{
+	System.out.println("yes");
+}
+ else if(num%i==0)
+	{
+		
+		System.out.println(" no");
+}
+else
+{
+			System.out.println("yes");
+}
+}
+}
