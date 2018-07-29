d502a8f473f1906ef4c4ba1759a51d6ba8ab8fbe
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-29 13:50:36 +0530

    Create primeint.java

---
+++ primeint.java
@@ -0,0 +1,22 @@
+class Primeint
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num,m,n;
+	Scanner sc=new Scanner(System.in);
+	m=sc.nextInt();
+	n=sc.nextInt();
+for(num=m;num<=n;num++)
+{
+if(num==2)
+{
+System.out.println("number is" +num);
+}
+else if(num%2==1)
+{
+		System.out.println("number is" +num);
+	} 
+// your code goes here
+}
+}
+}
