860ea698a09c049a85f42b74124ba9035774967f
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-08-17 10:58:32 +0530

    Create Int.java

---
+++ Int.java
@@ -0,0 +1,45 @@
+
+ import java.util.*; 
+import java.lang.*; 
+  import java.io.*;
+ class Anm 
+ { 
+ 	public static void main (String[] args) throws java.lang.Exception 
+ 	{ 
+ 		Scanner sc=new Scanner(System.in); 
+ 		int N=sc.nextInt(); 
+int K=sc.nextInt(); 
+	int sum=0; 
+ 		int a[]= new int[N]; 
+ 		for(int i=0;i<a.length;i++) 
+ 		{ 
+ 			a[i]=sc.nextInt(); 
+ 		} 
+		 
+	for(int i=0;i<K;i++) 		{ 
+  			sum=sum+a[i]; 
+} 
+ 		System.out.println(sum); 
+ 		 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+►
+
+
+ 
+
+
+© 2018 GitHub, Inc.
+Terms
