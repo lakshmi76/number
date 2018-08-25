579c0af6459834863abebdf7a28449f88fce396f
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-08-25 11:00:00 +0530

    Create Strnu.java

---
+++ Strnu.java
@@ -0,0 +1,47 @@
+
+ import java.util.*; 
+ import java.lang.*; 
+  import java.io.*; 
+ import java.util.Scanner; 
+  class Strnu 
+  { 
+  	public static void main (String[] args) throws java.lang.Exception 
+ 	{ 
+  
+   
+         Scanner sc=new Scanner(System.in); 
+          String string=sc.next(); 
+boolean numeric = true; 
+try { 
+            Double num = Double.parseDouble(string); 
+          } catch (NumberFormatException e) { 
+             numeric = false; 
+         } 
+    
+          if(numeric) 
+              System.out.println(string + " yes"); 
+         else 
+             System.out.println(string + " no"); 
+      } 
+  } 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+►
+
+
+ 
+
+
+
