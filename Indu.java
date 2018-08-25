b7f3363fcb85f3489d1259f7a325987b7fcc53e2
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-08-25 11:12:45 +0530

    Create Indu.java

---
+++ Indu.java
@@ -0,0 +1,23 @@
+ import java.util.*; 
+ import java.lang.*; 
+ import java.io.*; 
+  class Ideone 
+{ 
+ 	public static void main (String[] args) throws java.lang.Exception 
+  	{ 
+ 	int i,n; 
+ 	Scanner sc=new Scanner(System.in); 
+n=sc.nextInt(); 
+ 	int a[]=new int[n]; 
+for(i=0;i<n;i++) 
+ 	{ 
+  		a[i]=sc.nextInt(); 
+ 	 
+ 	} 
+ for(i=0;i<n;i++) 
+  { 
+  	System.out.println(a[i]+" "+i); 
+  } 
+		 
+ 	} 
+  } 
