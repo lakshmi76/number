2093368c29b9a6956326f2bbe6b6782ebea098dd
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-08-30 10:10:30 +0530

    Create Char.java

---
+++ Char.java
@@ -0,0 +1,17 @@
+ import java.util.*; 
+ import java.lang.*; 
+  import java.io.*;   
+  class Ideone 
+  { 
+ 	public static void main (String[] args) throws java.lang.Exception 
+ 	{ 
+ 		Scanner sc = new Scanner(System.in); 
+    System.out.println("Please enter a string"); 
+  String input = sc.nextLine(); 
+        System.out.println(input); 
+        String str = input; 
+       String[] myString = str.split(" "); 
+         int length = str.length(); 
+         System.out.println(length); 
+}// your code goes here 
+ 	}
